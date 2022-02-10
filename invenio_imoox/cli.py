99b8ee98287bb8ce8e1ca8483df5dfa63156c9ea
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-02-10 15:36:14 +0100

    add create and publish

--- invenio_imoox/cli.py
+++ invenio_imoox/cli.py
@@ -8,11 +8,15 @@
 """Click command-line interface for `invenio-imoox` module."""
 
 import json
+import time
 
 import click
 import requests
+from flask.cli import with_appcontext
+from invenio_records_lom import current_records_lom
 
 from .converter import MoocToLOM
+from .utils import get_identity_from_user_by_email
 
 
 def get_records_from_imoox(endpoint: str) -> dict:
@@ -32,6 +36,18 @@ def convert(imoox_records: list) -> dict:
     return lom_records
 
 
+def create_then_publish(lom_record: dict, identity):
+    service = current_records_lom.records_service
+
+    draft = service.create(data=lom_record, identity=identity)
+
+    # to prevent the race condition bug.
+    # see https://github.com/inveniosoftware/invenio-rdm-records/issues/809
+    time.sleep(0.5)
+
+    return service.publish(id_=draft.id, identity=identity)
+
+
 @click.group()
 def imoox():
     """CLI-group for `invenio-imoox` commands."""
@@ -39,9 +55,17 @@ def imoox():
 
 
 @imoox.command("import")
+@with_appcontext
 @click.option("--endpoint", required=True)
-def import_from_imoox(endpoint):
+@click.option("--user-email", type=click.STRING, default="imoox@tugraz.at")
+def import_from_imoox(endpoint, user_email):
     """Import metadata from endpoint into the repository."""
+    identity = get_identity_from_user_by_email(email=user_email)
     imoox_records = get_records_from_imoox(endpoint)
     lom_records = convert(imoox_records)
+
+    for lom_record in lom_records:
+        record = create_then_publish(lom_record, identity)
+        print(record.id)
+
     return lom_records
