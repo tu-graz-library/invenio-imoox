cc409125b45a6704df2c113747d27cf9cab056b9
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-04-28 21:05:28 +0200

    chore: maintenance

--- invenio_imoox/__init__.py
+++ invenio_imoox/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2022 Graz University of Technology.
+# Copyright (C) 2021-2025 Graz University of Technology.
 #
 # invenio-imoox is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -11,4 +11,7 @@ from .ext import InvenioIMooX
 
 __version__ = "0.3.0"
 
-__all__ = ("__version__", "InvenioIMooX")
+__all__ = (
+    "InvenioIMooX",
+    "__version__",
+)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2021-2024 Graz University of Technology.
+# Copyright (C) 2021-2025 Graz University of Technology.
 #
 # invenio-imoox is free software; you can redistribute it and/or modify
 # it under the terms of the MIT License; see LICENSE file for more details.
@@ -14,19 +14,11 @@ fixtures are available.
 from collections.abc import Callable
 
 import pytest
-from flask import Flask
-
-from invenio_imoox import InvenioIMooX
+from _pytest.fixtures import FixtureFunctionMarker
+from invenio_app.factory import create_api as _create_api
 
 
 @pytest.fixture(scope="module")
-def create_app(instance_path: str) -> Callable:
+def create_app(instance_path: FixtureFunctionMarker) -> Callable:
     """Application factory fixture."""
-
-    def factory(**config: dict) -> Flask:
-        app = Flask("testapp", instance_path=instance_path)
-        app.config.update(**config)
-        InvenioIMooX(app)
-        return app
-
-    return factory
+    return _create_api
